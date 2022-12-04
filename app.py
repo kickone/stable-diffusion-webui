4603d121b88bada98fae2823cf506fa804de623f
camenduru <camenduru@users.noreply.huggingface.co>
2022-12-04 05:01:42 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
 import os
-
-os.system(f"python --version")
 os.system(f"pip uninstall gradio -y")
-os.system(f"pip show gradio")
-os.system(f"uname -r")
-os.system(f"nvidia-smi")
-os.system(f"pwd")
-
-
 os.system(f"pip install -q https://github.com/camenduru/stable-diffusion-webui-colab/releases/download/0.0.15/xformers-0.0.15.dev0+1515f77.d20221130-cp38-cp38-linux_x86_64.whl")
 os.system(f"git clone https://github.com/camenduru/stable-diffusion-webui /home/user/app/stable-diffusion-webui")
 os.system(f"wget -q https://huggingface.co/Linaqruf/anything-v3.0/resolve/main/Anything-V3.0-pruned.ckpt -O /home/user/app/stable-diffusion-webui/models/Stable-diffusion/Anything-V3.0-pruned.ckpt")
@@ -25,7 +17,4 @@ os.system(f"wget -q https://huggingface.co/camenduru/i15/resolve/main/sd-v1-5-in
 os.system(f"wget -q https://huggingface.co/camenduru/sd14/resolve/main/sd-v1-4.ckpt -O /home/user/app/stable-diffusion-webui/models/Stable-diffusion/sd-v1-4.ckpt")
 os.system(f"wget -q https://huggingface.co/hakurei/waifu-diffusion-v1-3/resolve/main/wd-v1-3-float32.ckpt -O /home/user/app/stable-diffusion-webui/models/Stable-diffusion/wd-v1-3-float32.ckpt")
 os.chdir("/home/user/app/stable-diffusion-webui")
-os.system(f"pwd")
-os.system(f"pip show gradio")
 os.system(f"python launch.py --force-enable-xformers")
-
