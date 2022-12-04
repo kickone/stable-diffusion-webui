7a84096ce8e93513b71f60aa1962fbd0f49d0626
camenduru <camenduru@users.noreply.huggingface.co>
2022-12-04 04:08:36 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
 
 os.system(f"python --version")
+os.system(f"pip uninstall gradio")
 os.system(f"pip show gradio")
 os.system(f"uname -r")
 os.system(f"nvidia-smi")
 os.system(f"pwd")
+
 
 os.system(f"pip install -q https://github.com/camenduru/stable-diffusion-webui-colab/releases/download/0.0.15/xformers-0.0.15.dev0+1515f77.d20221130-cp38-cp38-linux_x86_64.whl")
 os.system(f"git clone https://github.com/camenduru/stable-diffusion-webui /home/user/app/stable-diffusion-webui")
