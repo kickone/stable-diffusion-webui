4c3cb763d718e03902fb358f07fc8fe6bc702289
camenduru <camenduru@users.noreply.huggingface.co>
2022-12-05 11:05:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 os.system(f"pip install -q https://github.com/camenduru/stable-diffusion-webui-colab/releases/download/0.0.15/xformers-0.0.15.dev0+1515f77.d20221130-cp38-cp38-linux_x86_64.whl")
-os.system(f"git clone https://github.com/camenduru/stable-diffusion-webui /home/user/app/stable-diffusion-webui")
+os.system(f"git clone -b queue https://github.com/camenduru/stable-diffusion-webui /home/user/app/stable-diffusion-webui")
 os.system(f"wget -q https://huggingface.co/Linaqruf/anything-v3.0/resolve/main/Anything-V3.0-pruned.ckpt -O /home/user/app/stable-diffusion-webui/models/Stable-diffusion/Anything-V3.0-pruned.ckpt")
 os.system(f"wget -q https://huggingface.co/Linaqruf/anything-v3.0/resolve/main/Anything-V3.0.vae.pt -O /home/user/app/stable-diffusion-webui/models/Stable-diffusion/Anything-V3.0-pruned.vae.pt")
 os.chdir("/home/user/app/stable-diffusion-webui")
